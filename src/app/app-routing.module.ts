b1a8d35b31723668360e6de2f9c5cdb1657f75da
Nour <nour.sboui1993@gmail.com>
2020-03-23 23:59:25 +0100

    Revert "redirect to sign in"
    This reverts commit 4f5c84bd4c0941960efe7bdd496e6e1ad74a1759.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { SymptomsFormComponent } from './components/symptoms-form/symptoms-form.component';
 import { HomeComponent } from './components/home/home.component';
 import { DoctorComponent } from  './components/doctor/doctor.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
+import {SignInComponent} from './components/sign-in/sign-in.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/se-connecter', pathMatch: 'full'},
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'},
   {path: 'accueil', component: HomeComponent},
   {path: 'formulaire-symptômes', component: SymptomsFormComponent},
   {path: 'médecin', component: DoctorComponent },
