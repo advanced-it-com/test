934c051bb832e89a691a196d08ea4f76f99eaf60
Ayman Ben Amor <ayman92.benamor@gmail.com>
2020-03-23 23:37:11 +0100

    Implement sign-in doctor stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SymptomsFormComponent } from './components/symptoms-form/symptoms-form.component';
 import { HomeComponent } from './components/home/home.component';
 import { DoctorComponent } from  './components/doctor/doctor.component';
+import {SignInComponent} from './components/sign-in/sign-in.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/accueil', pathMatch: 'full'},
   {path: 'accueil', component: HomeComponent},
   {path: 'formulaire-symptômes', component: SymptomsFormComponent},
-  {path: 'médecin', component: DoctorComponent }
+  {path: 'médecin', component: DoctorComponent },
+  {path: 'se-connecter', component: SignInComponent }
 ];
 
 @NgModule({
