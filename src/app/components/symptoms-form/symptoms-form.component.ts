fe71aac637e5e0010cfac13f0c9bd42fc823e174
Samet Abdessalem <abdessalemsamet@gmail.com>
2020-03-23 22:28:04 +0000

    question slide

--- src/app/components/symptoms-form/symptoms-form.component.ts
+++ src/app/components/symptoms-form/symptoms-form.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {SymptomsformService} from '../../services/symptomsform.service';
 import {FormSymptomsModule} from '../../models/form-symptoms.module';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-symptoms-form',
   templateUrl: './symptoms-form.component.html',
-  styleUrls: ['./symptoms-form.component.scss']
+  styleUrls: ['./symptoms-form.component.scss'],
+  animations: [
+    trigger('EnterLeave', [
+      state('flyIn', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.5s 300ms ease-in')
+      ]),
+      /*transition(':leave', [
+        animate('0.3s ease-out', style({ transform: 'translateX(100%)' }))
+      ])*/
+    ]),
+    /*trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(-100%)'}),
+        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({transform: 'translateX(-100%)'}))
+      ])
+    ])*/
+  ]
 })
 export class SymptomsFormComponent implements OnInit {
   questions: FormSymptomsModule[];
   formScore = 0;
   messageCorona: string;
+  currentQuestionIndex = 0;
+  showedQuestions: FormSymptomsModule[] = [];
+  questionsNum = 0;
 
   constructor(private symptomsformService: SymptomsformService) { }
 
@@ -22,8 +47,33 @@ export class SymptomsFormComponent implements OnInit {
       this.symptomsformService.getQuestions().subscribe(
         data => {
           this.questions = data;
+          this.showedQuestions.push(this.questions[0]);
+          this.questionsNum = this.questions.length;
         }
       );
+  }
+
+  isNext() {
+   return !(this.currentQuestionIndex === (this.questionsNum - 1));
+  }
+  next() {
+    if (this.isNext()) {
+      this.showedQuestions.pop();
+      this.currentQuestionIndex = this.currentQuestionIndex + 1;
+      this.showedQuestions.push(this.questions[this.currentQuestionIndex]);
+    }
+  }
+
+  isPrevious() {
+    return !(this.currentQuestionIndex === 0);
+  }
+
+  previous() {
+    if (this.isPrevious()) {
+      this.showedQuestions.pop();
+      this.currentQuestionIndex = this.currentQuestionIndex - 1;
+      this.showedQuestions.push(this.questions[this.currentQuestionIndex]);
+    }
   }
 
    calculateScore(suggestionScore) {
