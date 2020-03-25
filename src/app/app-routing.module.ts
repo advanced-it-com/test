54be3a10b0ab5131c48fe8a4eb1f790fae917c2d
Nour <nour.sboui1993@gmail.com>
2020-03-25 12:31:27 +0100

    contact doctor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { SymptomsFormComponent } from './components/symptoms-form/symptoms-form.
 import { HomeComponent } from './components/home/home.component';
 import { DoctorComponent } from  './components/doctor/doctor.component';
 import {SignInComponent} from './components/sign-in/sign-in.component';
+import {ContactDoctorComponent} from './components/contact-doctor/contact-doctor.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/accueil', pathMatch: 'full'},
   {path: 'accueil', component: HomeComponent},
   {path: 'formulaire-symptômes', component: SymptomsFormComponent},
-  {path: 'médecin', component: DoctorComponent },
-  {path: 'se-connecter', component: SignInComponent }
+  {path: 'médecin', component: DoctorComponent},
+  {path: 'se-connecter', component: SignInComponent},
+  {path: 'contacter-un-médecin', component: ContactDoctorComponent}
 ];
 
 @NgModule({
